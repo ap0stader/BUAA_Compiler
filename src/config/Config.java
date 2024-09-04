4d796f768fd567cb049cf46e0d1d86468bc1ce2c
ap0stader <ap0stader@outlook.com>
2024-09-04 16:05:35 +0800

    feat[Syntax Analysis]: 开始错误处理部分

--- src/config/Config.java
+++ src/config/Config.java
@@ -11,12 +11,15 @@ public class Config {
     public static boolean lexerThrowable = true;
     // 语法分析，如果不允许抛出错误，默认处理方式为return null;
     public static boolean parserThrowable = true;
+    // 错误收集
+    public static boolean errorHandlingThrowable = true;
 
     /* 总共进行阶段数
        1. 词法分析，生成TokenList
        2. 语法分析，生成AST
+       3. 错误处理
      */
-    public static int stages = 2;
+    public static int stages = 3;
 
     // 词法分析后，是否输出TokenStream、输出的文件名是否输出行号等信息
     public static boolean dumpTokenList = false;
@@ -25,7 +28,11 @@ public class Config {
 
     // 语法分析后，是否输出AST、输出的文件名
     public static boolean dumpAST = true;
-    public static final String dumpASTFileName = outputFilename;
+    public static final String dumpASTFileName = "dump_ast.txt";
+
+    // 错误收集后，是否输出错误处理结果
+    public static boolean dumpErrorInfo = true;
+    public static final String dumpErrorInfoFileName = outputFilename;
 
     // 通过传递的参数设置全局配置
     public static void setConfigByArgs(String[] args) {
@@ -35,9 +42,11 @@ public class Config {
                 case "--no-all-throw" -> {
                     lexerThrowable = false;
                     parserThrowable = false;
+                    errorHandlingThrowable = false;
                 }
                 case "--no-lexer-throw" -> lexerThrowable = false;
                 case "--no-parser-throw" -> parserThrowable = false;
+                case "--no-error-handling-throw" -> errorHandlingThrowable = false;
                 // Lexical Analysis: -L
                 case "-L" -> {
                     stages = 1;
@@ -47,6 +56,11 @@ public class Config {
                 case "-S" -> {
                     stages = 2;
                     dumpAST = true;
+                }
+                // Error Handling: -E
+                case "-E" -> {
+                    stages = 3;
+                    dumpErrorInfo = true;
                 }
             }
         }
