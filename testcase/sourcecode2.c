1b520019ff06c5c1251fb77da4a332e1f5635ac1
ap0stader <ap0stader@outlook.com>
2024-09-17 10:50:34 +0800

    完成了B类的工作

--- testcase/sourcecode2.c
+++ testcase/sourcecode2.c
@@ -1,5 +1,57 @@
 #include "libsysy.h"
 
+/*
+    Filename: sourcecode2.c
+    Author: @Ap0stader
+    Date: 2024-09-16
+*/
+
+// COMMENT AREA START
+/*///****///*///****////****//*///****////****//*///****////****///****/
+//*////****//*///****////****//*///****////****//*///****////****//*///*
+/*///****////****//*///****////****//*///****////****//*///****////****/
+/*///***////****////****//*///****////****//*///****////****//*///****//
+///*///****////****//*///****////****//*///****////****//*///****////***
+///*///****////****///*///****////****///*///****////**/*///****////****
+// COMMENT AREA END
+
+const int inta = 1, intb = 2;
+// ConstDef 一维数组
+// ConstInitVal 一维数组初值
+const int intarraya[3] = {1, 2, 3};
+// VarDef 一维数组
+// InitVal 一维数组初值
+int intarrayb[(-inta + intb * 2)] = {inta, inta + intb};
+
+// FuncFParam 一维数组
+int intarrayfunc(int array[], int size) {
+    int i, result = 0;
+    for (i = 0; i < size; i = i + 1) {
+        result = result + array[i];
+    }
+    return result;
+}
+
+int intfunc(int number) {
+    return number * number;
+}
+
+
 int main() {
     printf("22371345");
+    int intc = 3;
+    int intarrayc[inta + intb] = {inta, intc};
+    int intarrayd[4];
+
+    // LVal 一维数组
+    intarrayd[0] = getint();
+    intarrayd[inta] = getint();
+    intarrayd[intc - 1] = getint();
+
+    // FuncRParams 一维数组整体传参
+    printf("\n%d %d", intarrayfunc(intarrayc, 3), intarrayfunc(intarrayd, 2));
+    // FuncRParams 一维数组部分传参
+    printf("\n%d %d", intfunc(intarraya[2]), intarrayb[2]);
+
+    return 0;
 }
--- testcase/sourcecode3.c
+++ testcase/sourcecode3.c
@@ -1,5 +1,61 @@
 #include "libsysy.h"
 
+/*
+    Filename: sourcecode3.c
+    Author: @Ap0stader
+    Date: 2024-09-16
+*/
+
+// COMMENT AREA START
+/*///****///*///****////****//*///****////****//*///****////****///****/
+///*///****////****//*///****////****//*///****////****//*///****////***
+//*////****//*///****////****//*///****////****//*///****////****//*///*
+/*///****////****//*///****////****//*///****////****//*///****////****/
+/*///***////****////****//*///****////****//*///****////****//*///****//
+///*///****////****///*///****////****///*///****////**/*///****////****
+// COMMENT AREA END
+
+const char chara = 'a', charb = 'b';
+const char chararraya[2] = {'c', 'd'};
+// ConstInitVal StringConst
+const char chararrayb[6] = "Hello";
+char chararrayc[(-1 + 2 * 2)] = {chara, charb};
+int inta = 1;
+
+char charfunc(char character) {
+    return character + 1;
+}
+
+void print(char array[], int size) {
+    int i;
+    printf("\n");
+    for (i = 0; i < size; i = i + 1) {
+        printf("%c", array[i]);
+    }
+    printf("\n");
+}
+
 int main() {
     printf("22371345");
+    char charc = 'c';
+    // InitVal StringConst
+    char chararrayd[4 + 2] = "World";
+    char chararraye[3] = {chara, charc};
+    char chararrayf[2];
+    int intb = 3;
+
+    chararrayf[0] = getchar();
+    chararrayf[inta] = getchar();
+    chararrayf[intb - 1] = getchar();
+
+    print(chararrayf, 3);
+    print(chararrayd, 5);
+
+    printf("%c %c %c", charfunc(chararraye[1]), charfunc(chararraya[0]), charfunc(chararrayb[2]));
+    printf("\n%d", chararraye[2]);
+
+    int temp;
+    temp = getint();
+    printf("%d", temp);
+    return 0;
 }
